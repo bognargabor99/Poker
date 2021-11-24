79d171a971641491cadb901202954e1f78ac1157
bognargabor99 <gaborbognar99@gmail.com>
2021-11-24 10:10:28 +0100

    Always generate SSL certificate

--- Backend/src/hu/bme/aut/onlab/poker/Application.kt
+++ Backend/src/hu/bme/aut/onlab/poker/Application.kt
@@ -2,9 +2,20 @@ package hu.bme.aut.onlab.poker
 
 import hu.bme.aut.onlab.poker.plugins.*
 import io.ktor.application.*
+import io.ktor.network.tls.certificates.*
 import kotlinx.coroutines.DelicateCoroutinesApi
+import java.io.File
 
-fun main(args: Array<String>) = io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>) {
+    val keyStoreFile = File("build/keystore.jks")
+    generateCertificate(
+        file = keyStoreFile,
+        keyAlias = "sampleAlias",
+        keyPassword = "foobar",
+        jksPassword = "foobar"
+    )
+    io.ktor.server.netty.EngineMain.main(args)
+}
 
 @DelicateCoroutinesApi
 fun Application.module() {
